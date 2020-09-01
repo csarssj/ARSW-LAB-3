82348ec768cff52b1919fefc5718aacc22ca0191
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-09-01 01:02:24 -0500

    Completando parte 5

---
+++ CINEMA I/src/main/java/edu/eci/arsw/cinema/filter/AvailabilityFilter.java
@@ -0,0 +1,15 @@
+package edu.eci.arsw.cinema.filter;
+
+import java.util.List;
+
+import edu.eci.arsw.cinema.model.CinemaFunction;
+
+public class AvailabilityFilter implements CFilter{
+
+	@Override
+	public List<CinemaFunction> Cfilter(List<CinemaFunction> functions, String filtro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CINEMA I/src/main/java/edu/eci/arsw/cinema/filter/GenreFilter.java
@@ -0,0 +1,21 @@
+package edu.eci.arsw.cinema.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.eci.arsw.cinema.model.CinemaFunction;
+
+public class GenreFilter implements CFilter {
+
+	@Override
+	public List<CinemaFunction> Cfilter(List<CinemaFunction> functions, String genre) {
+		List<CinemaFunction> f = new ArrayList<CinemaFunction>();
+		for(CinemaFunction c : functions) {
+			if(genre.equals(c.getMovie().getGenre()) ) {
+				f.add(c);
+			}
+		}
+		return f;
+	}
+
+}
